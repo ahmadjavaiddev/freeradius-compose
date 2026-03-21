21cfc3af138ac8ba0f9d05d76da9b0311184620a
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2026-03-22 02:12:12 +0500

    Add new code from old repo

--- freeradius/docker-entrypoint.sh
+++ freeradius/docker-entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 set -e
 
+echo "[entrypoint] Substituting environment variables..."
+
 sed -i \
   -e "s|__DB_HOST__|${DB_HOST}|g" \
   -e "s|__DB_PASS__|${DB_PASS}|g" \
   /etc/freeradius/mods-available/sql
 
+cp /etc/freeradius/mods-available/sql /etc/freeradius/mods-enabled/sql
+
 sed -i \
   -e "s|__RADIUS_SECRET__|${RADIUS_SECRET}|g" \
   /etc/freeradius/clients.conf
 
-# Also update the enabled copy
-cp /etc/freeradius/mods-available/sql /etc/freeradius/mods-enabled/sql
-
+echo "[entrypoint] Done. Starting FreeRADIUS..."
 exec "$@"
